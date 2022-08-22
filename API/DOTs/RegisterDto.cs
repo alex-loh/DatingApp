2f8fdd610b44157bcf593531e440c8f88f5d9160
Alex Loh <alex.chloh@gmail.com>
2022-08-21 17:51:39 -0700

    End of section 7

--- API/DOTs/RegisterDto.cs
+++ API/DOTs/RegisterDto.cs
@@ -12,6 +12,7 @@ namespace API.DOTs
         public string Username { get; set; }
 
         [Required]
+        [StringLength(8, MinimumLength = 4)]
         public string Password { get; set; }
     }
 }
